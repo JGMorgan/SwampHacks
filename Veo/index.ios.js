22d4e6c03c902fa0783302a774205b9f8b8ef9e3
Jose Morgan <josegm11194@gmail.com>
2017-01-21 11:35:08 -0500

    added sample camera app to work with backend

--- Veo/index.ios.js
+++ Veo/index.ios.js
@@ -3,32 +3,37 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+'use strict'
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Dimension,
   Text,
   View
 } from 'react-native';
+import Camera from 'react-native-camera';
 
 export default class Veo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, please edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    takePicture() {
+    this.camera.capture()
+        .then((data) => console.log(data))
+        .catch(err => console.error(err));
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    style={styles.preview}
+                    aspect={Camera.constants.Aspect.fill}>
+                    <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+                </Camera>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -48,6 +53,21 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 });
 
 AppRegistry.registerComponent('Veo', () => Veo);
