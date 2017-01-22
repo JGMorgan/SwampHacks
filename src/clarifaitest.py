55d7477219a642621703b870565028519a00efc6
Jose Morgan <josegm11194@gmail.com>
2017-01-21 20:46:16 -0500

    clarifai stuff

--- src/clarifaitest.py
+++ src/clarifaitest.py
@@ -2,24 +2,39 @@ from clarifai import rest
 #from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
 from clarifai.rest import ClarifaiApp
+from flask import Flask
+from flask import request
 
-with open('../env/api_keys.txt') as f:
-    content = f.readlines()
 
-content = [x.strip() for x in content]
-app = ClarifaiApp(content[0], content[1])
+app = Flask(__name__)
 
-# get the general model
-#model = app.models.get("{model_id}")
+@app.route("/")
+def hello():
+    print request.data
 
-# predict with the model
-model = app.models.get('general-v1.3')
 
-import json
-link = 'http://nacto.org/wp-content/themes/sink_nacto/views/design-guides/retrofit/urban-street-design-guide/images/conventional-crosswalks/carousel//crystalcity_nn.jpg'
 
-print link
-data = json.dumps(model.predict_by_url(url=link)['outputs'])
-outputs = json.loads(data)[0]['data']['concepts']
-for element in outputs:
-    print element
+def train_clarifai():
+    with open('../env/api_keys.txt') as f:
+        content = f.readlines()
+
+    content = [x.strip() for x in content]
+    app = ClarifaiApp(content[0], content[1])
+
+    # get the general model
+    #model = app.models.get("{model_id}")
+
+    # predict with the model
+    model = app.models.get('general-v1.3')
+
+    import json
+    link = 'http://nacto.org/wp-content/themes/sink_nacto/views/design-guides/retrofit/urban-street-design-guide/images/conventional-crosswalks/carousel//crystalcity_nn.jpg'
+
+    print link
+    data = json.dumps(model.predict_by_url(url=link)['outputs'])
+    outputs = json.loads(data)[0]['data']['concepts']
+    for element in outputs:
+        print element
+
+if __name__ == "__main__":
+    app.run()
